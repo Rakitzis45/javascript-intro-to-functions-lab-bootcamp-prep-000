150dfbb96d0c21caa8d9c6f2f8eb9d6d08c62e48
Michael Rakitzis <michael.rakitzis@gmail.com>
2020-08-12 16:03:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 function shout(string) {
   return string.toUpperCase()
-}
+};
  function whisper(string) {
    return string.toLowerCase()
+ };
+ function logShout(string) {
+   return string.toUpperCase
  }
-console.log(shout("hello"))
